e5977929bb25547a4bd3a22acb6e7d901722a840
Hoang Tuan Anh <tuananhit91@gmail.com>
2015-07-07 19:42:39 +0700

    Initial Commit

---
+++ IAmRich/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IAmRich
+//
+//  Created by admin on 7/7/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
